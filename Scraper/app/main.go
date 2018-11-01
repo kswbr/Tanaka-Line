dde5cd2e6b23f51ca227d8350cda17b1cca7aef9
bugwabara <m.kashiwabara5963@gmail.com>
2018-11-01 11:40:43 +0900

    まずはサイトのHTMLを取得

--- Scraper/app/main.go
+++ Scraper/app/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "net/http"
+    "io/ioutil"
+    "fmt"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, from Docker container!")
+  url := "https://kushi-tanaka.com/news/"  // アクセスするURL
+
+  resp, err := http.Get(url)      // GETリクエストでアクセスする
+  if err != nil {                 // err ってのはエラーの時にエラーの内容が入ってくる
+      panic(err)                  // panicは処理を中断してエラーの中身を出力する
+  }
+  defer resp.Body.Close()         // 関数が終了するとクローズする
+
+  byteArray, err := ioutil.ReadAll(resp.Body) // 帰ってきたレスポンスの中身を取り出す
+  if err != nil {
+      panic(err)
+  }
+  fmt.Fprintf(w, string(byteArray))
 }
